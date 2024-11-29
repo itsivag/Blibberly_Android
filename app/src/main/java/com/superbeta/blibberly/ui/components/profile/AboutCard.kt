b0cd5f2d839c24c1340627d63c5a5f3464ee67f1
itsivag <sivacbrf2@gmail.com>
2024-11-29 20:25:56 +0530

    changed home ui

--- app/src/main/java/com/superbeta/blibberly/ui/components/profile/AboutCard.kt
+++ app/src/main/java/com/superbeta/blibberly/ui/components/profile/AboutCard.kt
@@ -1,32 +1,42 @@
 package com.superbeta.blibberly.ui.components.profile
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.superbeta.blibberly_auth.user.data.model.UserDataModel
 
 @Composable
 fun AboutCard(user: UserDataModel) {
     Card(
+        colors = CardColors(
+            containerColor = Color.White,
+            contentColor = Color.Black,
+            disabledContainerColor = Color.LightGray,
+            disabledContentColor = Color.DarkGray
+        ),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
+            .padding(12.dp)
     ) {
         Text(
             text = "About me",
+            color = Color.DarkGray,
             style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier.padding(bottom = 8.dp)
         )
 
         Text(
             text = user.aboutMe,
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.padding(start = 12.dp, end = 12.dp, bottom = 12.dp, top = 4.dp)
+            style = MaterialTheme.typography.titleLarge,
+//            modifier = Modifier.padding(bottom = 12.dp)
         )
     }
 }
--- app/src/main/java/com/superbeta/blibberly/ui/components/profile/InterestsCard.kt
+++ app/src/main/java/com/superbeta/blibberly/ui/components/profile/InterestsCard.kt
@@ -1,15 +1,18 @@
 package com.superbeta.blibberly.ui.components.profile
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.google.gson.Gson
 
@@ -19,30 +22,39 @@ fun InterestsCard(interests: String) {
     val interestsList = Gson().fromJson(interests, Array<String>::class.java)
 
     Card(
+        colors = CardColors(
+            containerColor = Color.White,
+            contentColor = Color.Black,
+            disabledContainerColor = Color.LightGray,
+            disabledContentColor = Color.DarkGray
+        ),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
+            .padding(12.dp)
     ) {
         Text(
             text = "Interests",
             style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(8.dp)
-        )
+//            modifier = Modifier.padding(8.dp)
+            color = Color.DarkGray
+
+            )
 
         LazyRow(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+//                .padding(bottom = 8.dp),
         ) {
             items(count = interestsList.size) { index ->
                 Text(
                     text = interestsList[index],
-                    style = MaterialTheme.typography.bodyMedium,
+                    style = MaterialTheme.typography.bodyLarge,
                     modifier = Modifier
-                        .padding(4.dp)
-                        .background(
+                        .padding(8.dp)
+                        .border(
+                            width = 1.dp,
                             color = MaterialTheme.colorScheme.secondary,
-                            shape = RoundedCornerShape(20.dp)
+                            shape = RoundedCornerShape(16.dp)
                         )
                         .padding(vertical = 4.dp, horizontal = 8.dp)
                 )
